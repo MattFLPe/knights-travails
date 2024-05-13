89e82d7c608702a1474ca7ece29f99e0d735be19
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-12 23:56:08 -0300

    Create function generateMoves: This function generates valid next moves from a given position

--- knightstravails.js
+++ knightstravails.js
@@ -1,3 +1,14 @@
 function knightMoves() {
-    
-}
+    const moves = [
+        [1, 2], [-1, 2], [2, 1], [-2, 1],
+    [1, -2], [-1, -2], [2, -1], [-2, -1]
+];
+
+    // Function to generate valid next moves from a given position
+    function generateMoves(pos) {
+        const [x, y] = pos;
+        const nextMoves = moves.map(([dx, dy]) => [x + dx, y + dy]);
+        return nextMoves.filter(([nx, ny]) => nx >= 0 && nx < 8 && ny >= 0 && ny < 8);
+    }
+
+};
